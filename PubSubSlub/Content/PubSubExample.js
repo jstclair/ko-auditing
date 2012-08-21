12e45b2cb1a2cbb781cf9d6ec875a15d4e20eeff
John St. Clair <john.st.clair@statkraft.no>
2012-08-21 10:45:32 +0200

    intial implementation using knockout-postbox

---
+++ PubSubSlub/Content/PubSubExample.js
@@ -0,0 +1,8 @@
+﻿$(function () {
+    var vm = {
+        name: ko.observable("John").publishOn('vm'),
+        email: ko.observable("john.stclair@statkraft.com").publishOn('vm'),
+        log: ko.observable().subscribeTo('vm')
+    };
+    ko.applyBindings(vm, document.getElementById('vm1'));
+});
